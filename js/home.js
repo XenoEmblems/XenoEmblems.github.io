38ffd03fbf36f7fcceb49d4a42634cf9e3d43d0a
Eric Doyle <edoyle@drew.edu>
2016-12-15 17:41:43 -0500

    Using old site for now. New site still in progress

--- js/home.js
+++ js/home.js
@@ -2,15 +2,28 @@
 console.log("JavaScript on");
 
 $('a').on('click', function(){
-	// When something on the navbar is clicked
+   // When something on the navbar is clicked
    var target = $(this).attr('rel');
-   // the rel attribute holds the target for which div should open.
-   $("#"+target).show().siblings("div").hide();
-   // The selected div is then shown and the other divs then hide
-   console.log(target + " selected");
-   // if (target === 'home' || target === 'resume' || target === 'contact'){
-   // 	document.body.style.backgroundColor = "#3B5998";
-   // } else {
-   // 	document.body.style.backgroundColor = "#ffa500";
-   // }
+
+   if (window.matchMedia("(min-width: 750px)").matches) {
+     /* the viewport is at least 750 pixels wide */
+     
+      // the rel attribute holds the target for which div should open.
+      $("#"+target).show().siblings("div").hide();
+      // The selected div is then shown and the other divs then hide
+      console.log(target + " selected");
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      if (target === 'home' || target === 'resume' || target === 'contact'){
+         document.body.style.backgroundColor = "#6495ED";
+      } else {
+         document.body.style.backgroundColor = "#2CA58D";
+      }
+   } else {
+      $('html, body').animate({
+        scrollTop: $("#"+target).offset().top
+      }, 2000);
+      document.body.style.backgroundColor = "#6495ED";
+   }
+  // 
 });
+
